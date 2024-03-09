3b12bbdfbc7e99782b7c5758a4ee6624c6ea3c65
Likkzz <143434628+Likkzz@users.noreply.github.com>
2024-03-10 00:40:38 +0530

    made few changes

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,6 @@
 //= link_tree ../images
 //= link_tree ../builds
 //= link index.css
- //= link about.css
+//= link about.css
+//= link index.js
+//= link people.css
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'home/about', to: 'home#about', as: 'about'
-  get 'home/people', to: 'home#people', as: 'people'
+  get 'home/People', to: 'home#People', as: 'People'
   # Defines the root path route ("/")
   # root "articles#index"
 end
